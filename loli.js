6c23edef8bcad120de9358c9e53f377502c8d3f7
zhangrongxiang <50192748+zhangrongxiang@users.noreply.github.com>
2021-02-05 16:45:20 +0800

    Site updated: 2021-02-05 16:45:11

--- loli.js
+++ loli.js
@@ -14,8 +14,8 @@ try {
                 dataType:"script",
                 cache:!0,
                 success:function() {
-                    live2d_settings.hitokotoAPI = "hitokoto.cn", live2d_settings.modelId = 1, live2d_settings.modelTexturesId = 81, live2d_settings.waifuEdgeSide = 'left:50',
-                    live2d_settings.modelStorage = !1, initModel("https://cdn.jsdelivr.net/gh/zhangrongxiang/zhangrongxiang.github.io@latest/live2d/assets/waifu-tips.json");
+                     live2d_settings.modelId = 1, live2d_settings.modelTexturesId = 81,
+                     initModel("https://cdn.jsdelivr.net/gh/zhangrongxiang/zhangrongxiang.github.io@latest/live2d/assets/waifu-tips.json");
                 }
             });
         }
