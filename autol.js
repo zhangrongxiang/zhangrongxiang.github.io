ed4cb2a1d0980eaa31c256c4a227765814ea4639
zhangrongxiang <50192748+zhangrongxiang@users.noreply.github.com>
2021-02-05 16:36:03 +0800

    Site updated: 2021-02-05 16:35:41

--- autol.js
+++ autol.js
@@ -1,21 +1,21 @@
 try {
     $("<link>").attr({
-        href:"https://cdn.jsdelivr.net/gh/zhangrongxiang/zhangrongxiang.github.io/live2d/assets/waifu.min.css",
+        href:"https://cdn.jsdelivr.net/gh/zhangrongxiang/zhangrongxiang.github.io@latest/live2d/assets/waifu.min.css",
         rel:"stylesheet",
         type:"text/css"
     }).appendTo("head"), $("body").append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>'), 
     $.ajax({
-        url:"https://cdn.jsdelivr.net/gh/zhangrongxiang/zhangrongxiang.github.io/live2d/assets/waifu-tips.js",
+        url:"https://cdn.jsdelivr.net/gh/zhangrongxiang/zhangrongxiang.github.io@latest/live2d/assets/waifu-tips.js",
         dataType:"script",
         cache:!0,
         success:function() {
             $.ajax({
-                url:"https://cdn.jsdelivr.net/gh/zhangrongxiang/zhangrongxiang.github.io/live2d/assets/live2d.min.js",
+                url:"https://cdn.jsdelivr.net/gh/zhangrongxiang/zhangrongxiang.github.io@latest/live2d/assets/live2d.min.js",
                 dataType:"script",
                 cache:!0,
                 success:function() {
                     live2d_settings.hitokotoAPI = "hitokoto.cn", live2d_settings.modelId = 1, live2d_settings.modelTexturesId = 81, live2d_settings.waifuEdgeSide = 'left:50',
-                    live2d_settings.modelStorage = !1, initModel("https://cdn.jsdelivr.net/gh/zhangrongxiang/zhangrongxiang.github.io/live2d/assets/waifu-tips.json");
+                    live2d_settings.modelStorage = !1, initModel("https://cdn.jsdelivr.net/gh/zhangrongxiang/zhangrongxiang.github.io@latest/live2d/assets/waifu-tips.json");
                 }
             });
         }
--- medias/banner/rename.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-set count=0
-for /f %%i in ('dir /b *.jpg') do (
-    set /a count+=1
-    echo 改名：%%i !count!
-    rename %%i !count!.jpg
-)
