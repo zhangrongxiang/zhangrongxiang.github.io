e3859f14fba4b3e7274e1662f78cd519b1f6ff41
zhangrongxiang <50192748+zhangrongxiang@users.noreply.github.com>
2021-02-06 11:14:18 +0800

    Site updated: 2021-02-06 11:13:58

--- loli.js
+++ loli.js
@@ -5,7 +5,7 @@ try {
         type:"text/css"
     }).appendTo("head"), $("body").append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>'), 
     $.ajax({
-        url:"https://cdn.jsdelivr.net/gh/zhangrongxiang/zhangrongxiang.github.io@latest/live2d/assets/waifu-tips.js",
+        url:"https://cdn.jsdelivr.net/gh/zhangrongxiang/zhangrongxiang.github.io/live2d/assets/waifu-tips.js",
         dataType:"script",
         cache:!0,
         success:function() {
